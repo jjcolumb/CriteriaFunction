97f93289fe79c8adb05b7242065bd6c03cc45ee4
Jose Columbie <josejaviercolumbie@yahoo.com>
2021-02-09 23:18:48 -0700

    Add project files.

---
+++ CriteriaFunction.Module/BusinessObjects/DomainObject1.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace CriteriaFunction.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [RuleCriteria("test", DefaultContexts.Save, "UserName = GetCurrentUserName()")]
+    public class DomainObject1 : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        public DomainObject1(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            UserName = SecuritySystem.CurrentUserName;
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        string _propertyOne;
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string PropertyOne
+        {
+            get => _propertyOne;
+            set => SetPropertyValue(nameof(PropertyOne), ref _propertyOne, value);
+        }
+
+        string _userName;
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string UserName
+        {
+            get => _userName;
+            set => SetPropertyValue(nameof(UserName), ref _userName, value);
+        }
+
+    }
+}
---
+++ CriteriaFunction.Module/FunctionCriteria/CustomFunctionBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+
+namespace FunctionCriteria.Module.CriteriaFunction
+{
+    public class CustomFunctionBase : ICustomFunctionOperator
+    {
+        public virtual string Name { get { throw new NotImplementedException(); } }
+
+        public virtual object Evaluate(params object[] operands)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Type ResultType(params Type[] operands)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class GetCurrentUserNameFunction : CustomFunctionBase
+    {
+        public override string Name { get { return "GetCurrentUserName"; } }
+
+        public override object Evaluate(params object[] operands)
+        {
+            var user = SecuritySystem.CurrentUser as PermissionPolicyUser;
+            if (user != null)
+            {
+                return user.UserName;
+            }
+            else
+            {
+                return null;
+            }
+
+
+        }
+
+        public override Type ResultType(params Type[] operands)
+        {
+            return typeof(string);
+        }
+    }
+}
